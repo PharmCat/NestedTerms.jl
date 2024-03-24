77b6742ce23aed9b77befb2258b3a03ce044ddc8
Documenter.jl <documenter@juliadocs.github.io>
2024-03-24 20:24:03 +0000

    build based on a8c528f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NestedTerms","category":"page"},{"location":"#NestedTerms","page":"Home","title":"NestedTerms","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NestedTerms.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NestedTerms]","category":"page"}]
+}
